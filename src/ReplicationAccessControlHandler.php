df7f295d62e5849a4c940b21c8f916e0f9a64579
Andrei Jechiu <andrei.jechiu@gmail.com>
2019-04-03 11:33:08 +0300

    Update the message.

--- src/ReplicationAccessControlHandler.php
+++ src/ReplicationAccessControlHandler.php
@@ -101,7 +101,7 @@ class ReplicationAccessControlHandler extends EntityAccessControlHandler impleme
       ->execute();
     if (!empty($replication_in_queue)) {
       $this->messenger()
-        ->addWarning(t('A deployment between the active workspace and target workspace is currently queued or in progress. Creating a new deployment is not allowed until the current one ends. Check @deployments_page for the status.', [
+        ->addWarning(t('A deployment from the active workspace is currently queued or in progress. Creating a new deployment is not allowed until the current one ends. Check @deployments_page for the status.', [
           '@deployments_page' => Link::createFromRoute('Deployments page', 'entity.replication.collection')
             ->toString()
         ]));
